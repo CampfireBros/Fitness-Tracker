ffcd5f85a9f69b0f3e7142d4f2e02bbe85cb0010
Craig <ellis.cr@husky.neu.edu>
2016-06-02 20:13:06 -0400

    email error

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ TESTING = False
 
 #MAIL_USERNAME = "exercisedb@exercisedb.com"
 MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
+MAIL_PORT = 465
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 MAIL_USERNAME = 'theexercisedb@gmail.com'
